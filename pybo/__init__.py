00a874deaa9065ce217c965553eaf8c194203425
admin <admin@adminui-MacBookPro-2.local>
2024-03-02 17:23:33 +0900

    404 오류등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -19,6 +19,8 @@ migrate = Migrate()
 #db = SQLAlchemy()
 #migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
     app = Flask(__name__)
@@ -26,6 +28,8 @@ def create_app():
     #app.config.from_object(config)
     # markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
 
     # ORM
     db.init_app(app)
